5e5e154dfcc5297628ca1486f6963b9b27b49988
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-08-27 15:01:49 -0300

    se sube el video 3 de python

--- Python/Semana 2/Clase01/Sistemas_numeracion.py
+++ Python/Semana 2/Clase01/Sistemas_numeracion.py
@@ -24,4 +24,16 @@ print(f"a decimal: {a}")
 a=int('10111',2)
 print(f"a base binario: {a}")
 
+# Base Octal
+a = int('27', 8)
+print(f"a = base octal: {a}")
+
+# Base Hexadecimal
+a = int('17', 16)
+print(f"a = base hexadecimal: {a}")
+
+#Base 5 sus valores son de 0 a 4
+a = int ("34", 5)
+print(f"a = base 5:{a}")
+
 
