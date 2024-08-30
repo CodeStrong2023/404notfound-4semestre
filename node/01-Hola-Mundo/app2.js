8353d76fad85883c53823d03c9be8d3352054159
Enzo <enzoastorga98@gmail.com>
2024-08-29 21:53:42 -0300

    Video 1 y 2 Nodejs / Clase 03

---
+++ node/01-Hola-Mundo/app2.js
@@ -0,0 +1,5 @@
+const saludar = (nombre) =>{ // Función de flecha, recibe un argumento.
+    return `Saludos de ${nombre}`;  // Back tic`s temple de string.
+}  // Esto es igual a: 'Saludos' + nombre;
+console.log(saludar('Ariel'));
+
