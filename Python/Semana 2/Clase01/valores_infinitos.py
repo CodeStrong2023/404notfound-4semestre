657551384637e86dfd48a668268597b3919bc606
Javier <javii07bj@gmail.com>
2024-08-30 21:44:36 -0300

    Agrego valor infinito positivo y negativo parte 7

--- Python/Semana 2/Clase01/valores_infinitos.py
+++ Python/Semana 2/Clase01/valores_infinitos.py
@@ -1,4 +1,6 @@
 import math
+from decimalimport Decimal
+
 #Manejo de valores infinitos
 infinito_positivo = float('inf')
 print(f'Infinito positivo: {infinito_positivo}')
@@ -7,3 +9,21 @@ print(f'Es infinito?: {math.isinf(infinito_positivo)}')
 infinito_negativo = float('-inf')
 print(f'Infinito negativo: {infinito_negativo}')
 print(f'Es infinito?: {math.isinf(infinito_negativo)}')
+
+# Modulo match
+infinito_positivo = match.inf
+print(f'Infinito positivo: {infinito_positivo}')
+print(f'Es infinito?: {math.isinf(infinito_positivo)}')
+
+infinito_negativo = -match.inf
+print(f'Infinito negativo: {infinito_negativo}')
+print(f'Es infinito?: {math.isinf(infinito_negativo)}')
+
+# Modulo decimal
+infinito_positivo= Decimal("Infinity")
+print(f"Infinito positivo: {infinito_positivo}")
+print(f"Es infinito?": {math.isinf(infinito_positivo)}")
+
+infinito_negativo = Decimal("-Infinity")
+print(f"Infinito negativo: {infinito_negativo}")
+print(f"Es infinito": {math.isinf(infinito_negativo)}")
