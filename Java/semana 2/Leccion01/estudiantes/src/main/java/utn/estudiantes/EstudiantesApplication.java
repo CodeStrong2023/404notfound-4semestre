c977141352e75f15e7b0d9c1b8a672fcbbff70a6
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-09-13 20:21:45 -0300

    Agregamos video 3 y 4 de la clase 5

--- Java/semana 2/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/semana 2/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import utn.estudiantes.modelo.Estudiante2022;
 import utn.estudiantes.servicio.EstudianteServicio;
 
+import java.util.List;
+import java.util.Scanner;
+
 @SpringBootApplication  // Anotación para ejecutar
 public class EstudiantesApplication implements CommandLineRunner { // Ejecutamos la app por consola e implementamos el método.
 
@@ -27,5 +30,38 @@ public class EstudiantesApplication implements CommandLineRunner { // Ejecutamos
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info(nl+"Ejecutando el método run de Spring..."+nl);
+		var salir = false;
+		var consola = new Scanner(System.in);
+		while(!salir!){
+			mostrarMenu();
+			salir = ejecutarOpciones(consola);
+			logger.info(nl);
+		}//Fin del ciclo while
+	}
+
+	private void mostrarMenu(){
+		logger.info(nl);
+		logger.info("""
+				******* Sistema de Estudiantes *******
+				1. Listar Estudiantes
+				2. Buscar Estudiante
+				3. Agregar Estudiante
+				4. Modificar Estudiante
+				5. Eliminar Estudiante
+				6. Salir 
+				Elija una opción:""");
+	}
+
+	private boolean ejecutarOpciones(Scanner consola){
+		var opcion = Integer.parseInt(consola.nextLine());
+		var salir = false;
+		switch (opcion){
+			case 1 -> {
+				logger.info(nl+"Listado de Estudiantes: "+nl);
+				List<Estudiante2022> estudiantes = estudianteServicio.listarEstudiantes();
+				estudiantes.forEach((estudiante -> logger.info(estudiante.toString()+nl)));
+			}
+		}//Fin swich
+		return salir;
 	}
 }
