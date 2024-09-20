ec9ea6d660c893716401e08fcbff73b983dd5257
Maximiliano Robilotta <maxirobilottapons@gmail.com>
2024-09-20 19:07:48 -0300

    Video 2 Clase 6 Node

--- node/01-Hola-Mundo/entorno.js
+++ node/01-Hola-Mundo/entorno.js
@@ -1,3 +1,5 @@
-let nombre = process.env.NOMBRE;
+let nombre = process.env.NOMBRE || "Sin nombre";
+let web = process.env.WEB || "No tengo web";
 
 console.log("hola " + nombre);
+console.log("Mi web es: " + web);
