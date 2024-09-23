13fce964bb4e348077642388a7ad5c8e0ff6a21b
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-09-23 13:28:17 -0300

    Añadiendo cosas al servidor parte 2 js

--- JavaScript/Semana 6/PERN-stack/src/app.js
+++ JavaScript/Semana 6/PERN-stack/src/app.js
@@ -1,6 +1,7 @@
 import e from "express";
 import express from "express";
 import tareasRoutes from "./router/tareas.routes.js"
+import authRoutes from "./router/auth.routes.js"
 const app = express();
 //Middlewars
 app.use(morgan("dev"));
@@ -8,7 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.json({messaje: "Bienbenidos a mi proyecto"}));
-app.use("/tareas", tareasRoutes);
+app.use('/api', tareasRoutes);
+app.use('/api', authRoutes)
 
 //manejando errores
 app.use((req, res, next) => {
