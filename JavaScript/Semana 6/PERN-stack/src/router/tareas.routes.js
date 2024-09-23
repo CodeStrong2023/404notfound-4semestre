e294215a8d00f00bb6919a0eecb4aba97b1544c0
Matias Landi <landi.matias7@gmail.com>
2024-09-23 19:25:17 -0300

    Agrego video 12 de la clase 5 de JavaScript

--- JavaScript/Semana 6/PERN-stack/src/router/tareas.routes.js
+++ JavaScript/Semana 6/PERN-stack/src/router/tareas.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
+import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controller.js"
 
 const router = Router();
 
-router.get("/tareas",(req, res) => res.send("obteniendo tareas"));
+router.get("/tareas", listarTareas);
 
-router.get("/tareas/:id",(req, res) => res.send("obteniendo tarea unica"));
+router.get("/tareas/:id", listarTarea);
 
-router.post("/tareas",(req, res) => res.send("creando tarea"));
+router.post("/tareas", crearTarea);
 
-router.put("/tareas/:id",(req, res) => res.send("actualizando tarea unica"));
+router.put("/tareas/:id", actualizarTarea);
 
-router.delete("/tareas/:id",(req, res) => res.send("eliminando tarea unica"));
+router.delete("/tareas/:id", eliminarTarea);
 
 export default router;
