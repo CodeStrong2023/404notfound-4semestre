70e82cd4dc79013ac49bf12fda974804b92b4ad8
Enzo <enzoastorga98@gmail.com>
2024-09-23 23:21:02 -0300

    Arreglo ecommerce

--- JavaScript/Semana1/E-COMMERCE/server/node_modules/mercadopago/dist/clients/preApprovalPlan/update/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { UpdatePreApprovalPlanUpdateClient } from './types';
-import type { PreApprovalPlanResponse } from '../../../clients/preApprovalPlan/commonTypes';
-export default function update({ id, updatePreApprovalPlanRequest, config }: UpdatePreApprovalPlanUpdateClient): Promise<PreApprovalPlanResponse>;
--- JavaScript/Semana1/E-COMMERCE/server/node_modules/mercadopago/dist/utils/types.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type RestClientConfig = {
-    timeout?: number;
-    queryParams?: Record<string, string | number>;
-};
