660d00b4e57ef41477736d8e9591052cd7d3587b
Enzo <enzoastorga98@gmail.com>
2024-09-23 23:26:02 -0300

    Arreglo ecommerce

--- JavaScript/Semana1/E-COMMERCE/server/node_modules/mercadopago/dist/clients/point/getPaymentIntentStatus/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { PaymentIntentStatusResponse } from '../commonTypes';
-import type { PointGetPaymentIntentStatusClient } from './types';
-export default function getPaymentIntentStatus({ payment_intent_id, config }: PointGetPaymentIntentStatusClient): Promise<PaymentIntentStatusResponse>;
--- JavaScript/Semana1/E-COMMERCE/server/node_modules/mercadopago/dist/clients/preference/create/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { PreferenceCreateClient } from './types';
-import type { PreferenceResponse } from '../../../clients/preference/commonTypes';
-export default function create({ body, config }: PreferenceCreateClient): Promise<PreferenceResponse>;
