12d232e813c2a5da3c9938fef83d9b5686752e85
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-09-26 21:33:47 -0300

    se sube el video 5 de java

--- Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/servicio/LibroServicio.java
+++ Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/servicio/LibroServicio.java
@@ -1,4 +1,34 @@
 package utn.tienda_libros.servicio;
 
-public class LibroServicio {
+import org.springframeword.stereotype.Service;
+
+@Service
+
+public class LibroServicio implements ILibroServicio {
+
+    @Autowired
+    private LibroRepsitorio libroRepsitorio;
+
+    @Override
+    public List<Libro> listarLibros() {
+        return libroRepositorio.findAll();
+    }
+
+    @Override
+    public Libro buscarLibroPorId(Integer idLibro) {
+        Libro libro = libroRepsitorio.findById(idLibro).orElse(null);
+        return libro;
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+        libroRepositorio.save(libro);
+    }
+
+    @Override
+    public void eliminarLibro(Libro libro) {
+        libroRepositorio.delete(libro);
+    }
 }
+
+
