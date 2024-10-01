e18dce4a23512bb5a08c7eacfae3c49003178012
Matias Landi <landi.matias7@gmail.com>
2024-10-01 16:43:51 -0300

    Agrego video 2 de la clase 7 de JavaScript

--- JavaScript/Semana 6/PERN-stack/src/controllers/auth.controller.js
+++ JavaScript/Semana 6/PERN-stack/src/controllers/auth.controller.js
@@ -2,12 +2,18 @@ import {pool} from  "../db.js"
 
 export const signin = (req, res)=> res.send("Ingresando");
 
-export const signup = (req, res)=> {
+export const signup = async(req, res)=> {
     const {name, email, password} = req.body;
+    console.log(name, email, password);
     res.send("Registrado");
 
-    const result = await pool.query("INSERT INTO usuarios (name, email,password) VALUES ($1, $2, $3)", [name, email, password])
-    console.log(result);
+    try {
+        const result = await pool.query("INSERT INTO usuarios (name, email,password) VALUES ($1, $2, $3)", [name, email, password])
+        console.log(result);
+        return res.json(result.rows[0]);
+    } catch (error) {
+        
+    }
 };
 
 export const signout = (req, res)=> res.send("Cerrando sesion");
