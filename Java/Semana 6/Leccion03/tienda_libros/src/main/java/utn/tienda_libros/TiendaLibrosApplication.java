9cda9298bf18e020ae176f79f689a985a65ca330
Matias Landi <landi.matias7@gmail.com>
2024-10-03 19:38:02 -0300

    Agrego video 2 de la clase 8 de Java

--- Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,7 +1,11 @@
 package utn.tienda_libros;
 
+import org.springFramework.boot.WebApplicationType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
@@ -9,6 +13,15 @@ public class TiendaLibrosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaLibrosApplication.class, args);
 	}
-	ConfigurationApplicationContext contextoSpring=
-	new SpringApplicationBuilder(TiendaLibrosApplication.class);
+		ConfigurationApplicationContext contextoSpring=
+			new SpringApplicationBuilder(TiendaLibrosApplication.class)
+				.headless(false)
+				.web(WebApplicationType.NONE)
+				.run(args);
+
+		//Ejecutamos el código para cargar el formulario
+		EventQueue.invokeLater(() -> {
+			//Obtenemos el objeto from a través del spring
+			LibroFrom libroFrom = contextoSpring.getBrean(LibroFrom.class);
+	});
 }
