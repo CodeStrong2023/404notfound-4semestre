d454621e002ad77e92812a1c8a4e5470a7a24d97
Enzo Yamil Astorga <enzoastorga98@gmail.com>
2024-10-03 20:56:52 -0300

    Video 3 Clase 08 JAVA

--- Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,27 +1,30 @@
 package utn.tienda_libros;
 
-import org.springFramework.boot.WebApplicationType;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
+
 import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
-		ConfigurationApplicationContext contextoSpring=
+
+		ConfigurableApplicationContext contextoSpring=
 			new SpringApplicationBuilder(TiendaLibrosApplication.class)
 				.headless(false)
 				.web(WebApplicationType.NONE)
 				.run(args);
 
 		//Ejecutamos el código para cargar el formulario
-		EventQueue.invokeLater(() -> {
+		EventQueue.invokeLater(() -> {  // Método Lambda
 			//Obtenemos el objeto from a través del spring
-			LibroFrom libroFrom = contextoSpring.getBrean(LibroFrom.class);
-	});
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			libroFrom.setVisible(true);
+		});
+	}
 }
