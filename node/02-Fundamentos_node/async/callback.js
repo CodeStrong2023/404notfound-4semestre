f32280cd6147256d154634a2036f57239bfb920b
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-10-03 23:34:49 -0300

    Comenzamos creando carpeta, archivo y codigo node

---
+++ node/02-Fundamentos_node/async/callback.js
@@ -0,0 +1,7 @@
+function soyAsincrona(){
+    console.log('Hola, soy una función asíncrona');
+}
+
+console.log('Iniciando el proceso...');
+soyAsincrona();
+console.log('Terminando el proceso...');
