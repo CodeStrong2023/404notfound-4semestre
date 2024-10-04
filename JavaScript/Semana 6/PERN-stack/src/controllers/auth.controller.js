1a2f26337d2a3236dc928b9da426c10c958f6fb8
Javier <javii07bj@gmail.com>
2024-10-04 00:14:07 -0300

    Proyecto con stack pern, Java Script

--- JavaScript/Semana 6/PERN-stack/src/controllers/auth.controller.js
+++ JavaScript/Semana 6/PERN-stack/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import {pool} from  "../db.js"
 import bcrypt from "bcrypt";
+import {createAccessToken} from "../libs/jwt.js"
 
 export const signin = (req, res)=> res.send("Ingresando");
 
@@ -13,8 +14,17 @@ export const signup = async(req, res)=> {
         console.log(hashedPassword)
 
         const result = await pool.query("INSERT INTO usuarios (name, email,password) VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword])
+        const token = await createAccessToken([id: result.rows[0].id]);
+
+
+
         console.log(result);
-        return res.json(result.rows[0]);
+        // return res.json(result.rows[0]);
+        return res.json({
+            token: token,
+        }
+
+        )
     } catch (error) {
         if (error.code === "23505"){
             return res.status(400).json({messaje: "El correo ya esta registrado"});
--- JavaScript/Semana 6/PERN-stack/src/libs/jwt.js
+++ JavaScript/Semana 6/PERN-stack/src/libs/jwt.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const createAccessToken = (paylod) => {
+export const createAccessToken = (paylod) => {
     return new Promise ((resolve, reject) => {  
         jwt.sign(paylod, "xyz123", { expiresIn: "1d"},
         (err, token) => {
