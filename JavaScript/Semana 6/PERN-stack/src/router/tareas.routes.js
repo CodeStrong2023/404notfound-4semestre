45cf4f5faee5942c922dafa55ae3e1fb6158b36a
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-10-08 10:00:56 -0300

    Creación de un middleware -> auth.middleware.js parte 2

--- JavaScript/Semana 6/PERN-stack/src/router/tareas.routes.js
+++ JavaScript/Semana 6/PERN-stack/src/router/tareas.routes.js
@@ -1,9 +1,10 @@
 import Router from "express-promise-router";
 import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controller.js"
+import {isAuth} from "../middlewares/auth.middlewares.js"
 
 const router = Router();
 
-router.get("/tareas", listarTareas);
+router.get("/tareas", isAuth, listarTareas);
 
 router.get("/tareas/:id", listarTarea);
 
