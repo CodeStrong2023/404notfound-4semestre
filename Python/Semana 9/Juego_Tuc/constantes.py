ee64136a4c62016ea81f5498b8cdc7bf84393c91
ignacio jose rocha <ignac1997nacho@gmail.com>
2024-10-09 02:55:20 -0300

    agrego los videos

--- Python/Semana 9/Juego_Tuc/constantes.py
+++ Python/Semana 9/Juego_Tuc/constantes.py
@@ -4,3 +4,9 @@
 
 SCREEN_WIDTH = 1700
 SCREEN_HEIGTH = 1000
+
+#ruta de los assets
+ASSETS_PATH='C:/Users/Usuario/Documents/Python/Proyectos/PyGame/Semana 9/Juego_Tuc/assets'
+
+#ruta de los archivos
+IMPERIAL_MARCH_PATH=f'{ASSETS_PATH}/audio/Imperial March.wav'
