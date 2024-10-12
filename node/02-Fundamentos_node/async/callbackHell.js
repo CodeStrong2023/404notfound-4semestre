236a2b89e05d41a57925ae918e924320886d33cd
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-10-11 23:23:15 -0300

    Introducción al callback hell

--- node/02-Fundamentos_node/async/callbackHell.js
+++ node/02-Fundamentos_node/async/callbackHell.js
@@ -5,7 +5,12 @@ function Hola(nombre, miCallback){
         miCallback(nombre);
     }, 1000);
 }
-
+function hablar(callbackHablar){
+    setTimeout(function(){
+        console.log('bla bla bla bla');
+        callbackHablar();
+    }, 1000);
+}
 function adios(nombre, otroCallback) {
     setTimeout(function(){
         console.log('Adios' + nombre);
@@ -13,13 +18,11 @@ function adios(nombre, otroCallback) {
     }, 1500)
 }
 
+// --Proceso Principal
 console.log('Iniciando el proceso...');
 Hola('Carlos',function() {
     adios('Carlos', function(){
         console.log('Terminando el proceso...');
     })
 });
-
-//hola('Carlos', function(){});
-//adios('Carlos', function(){});
 
