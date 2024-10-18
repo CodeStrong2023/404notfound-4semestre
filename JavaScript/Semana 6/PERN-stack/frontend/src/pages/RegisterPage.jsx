181355b8b164e1af33dab997392cbc477e0f085d
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-10-18 17:06:22 -0300

    Formulario de registro parte 2

--- JavaScript/Semana 6/PERN-stack/frontend/src/pages/RegisterPage.jsx
+++ JavaScript/Semana 6/PERN-stack/frontend/src/pages/RegisterPage.jsx
@@ -1,16 +1,39 @@
-import {Input} from "../components/ui/input";
-import {Card} from "../components/ui/Card";
-import {Link} from "../components/ui/Button"
+import { Button, Card, Input } from "../components/ui/Button";
+import { useForm } from "react-hook-form";
 function RegisterPage() {
+
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
 
     <div className="h-[calc(100vh-64px)] flex items-center ustufy-center">
          <Card>
          <h3 className='text-2xl font-bold text-blue-600'>Registro</h3>
-            <form>
-                <Input placeholder="Ingrese su nombre"></Input>
-                <Input type="email" placeholder="Ingrese su email"></Input>
-                <Input type="password" placeholder="Ingrese su contraseña"></Input>
+            <form onSubmit={onSubmit}>
+                <Input placeholder="Ingrese su nombre"
+                {...register("name", {required:true})}></Input>
+
+                {
+                  errors.name && <p className="text-red-500">Este campo es requerido</p>
+                }
+
+                <Input type="email" placeholder="Ingrese su email"
+                {...register("email", {required:true})}></Input>
+
+                {
+                  errors.email && <p className="text-red-500">Este campo es requerido</p>
+                }
+
+                <Input type="password" placeholder="Ingrese su contraseña"
+                {...register("password", {required:true})}></Input>
+
+                {
+                  errors.password && <p className="text-red-500">Este campo es requerido</p>
+                }
 
                 <Button>Registrarse</Button>
             </form>
