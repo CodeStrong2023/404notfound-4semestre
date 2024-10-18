2f013f6a699c9d35101fc75540080d65e6539bcd
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-10-18 14:21:10 -0300

    Agregamos formulario e registro js

---
+++ JavaScript/Semana 6/PERN-stack/frontend/src/components/ui/input.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Input(props){
+    return(
+        <input type="text" className= "bgz-zinc-800 px-3 py-2 block my-2 w-full"/>
+        {...props}/>
+        )
+    }
+
+export default Input
--- JavaScript/Semana 6/PERN-stack/frontend/src/pages/RegisterPage.jsx
+++ JavaScript/Semana 6/PERN-stack/frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import {Input} from "../components/ui/input";
+import {Card} from "../components/ui/Card";
+import {Link} from "../components/ui/Button"
 function RegisterPage() {
   return (
-    <h3 className='text-2xl font-bold text-yellow-300'>Registro</h3>
-  )
+
+    <div className="h-[calc(100vh-64px)] flex items-center ustufy-center">
+         <Card>
+         <h3 className='text-2xl font-bold text-blue-600'>Registro</h3>
+            <form>
+                <Input placeholder="Ingrese su nombre"></Input>
+                <Input type="email" placeholder="Ingrese su email"></Input>
+                <Input type="password" placeholder="Ingrese su contraseña"></Input>
+
+                <Button>Registrarse</Button>
+            </form>
+         </Card>
+    </div>
+  );
 }
 
 export default RegisterPage
