5d5c009d4c56394ae1e234b31d2055bf08c8b7f7
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-10-19 18:54:54 -0300

    se sube el video 5 de node

--- node/02-Fundamentos_node/async/promises.js
+++ node/02-Fundamentos_node/async/promises.js
@@ -20,7 +20,8 @@ function adios(nombre) {
     return new Promise((resolve, reject) => {
         setTimeout(function() {
             console.log('Adios ' + nombre);
-            resolve(); // No es necesario pasar el nombre en la última función
+            //resolve(); // No es necesario pasar el nombre en la última función
+            reject('Hay un error')
         }, 1000);
     });
 }
@@ -32,9 +33,10 @@ Hola('Ariel')
     .then(hablar)
     .then(hablar)
     .then(adios)
-    .then(() => {
+    .then((nombre) => {
         console.log('Terminando el proceso');
     })
     .catch((error) => {
-        console.error('Error:', error);
+        console.log('Ha habido un error');
+        console.log(error);
     });
