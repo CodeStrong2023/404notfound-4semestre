1ddb947e5166a66036cdb014471feab5325bb1f3
Sebas <sebasbeta-11@hotmail.com>
2024-10-19 16:28:43 -0300

    Modificando funcion hablar - video4

--- node/02-Fundamentos_node/async/promises.js
+++ node/02-Fundamentos_node/async/promises.js
@@ -5,16 +5,33 @@ function Hola(nombre, miCallback){
     }, 1000);
 }
 
-function hablar(callbackHablar){
-    setTimeout(function(){
-        console.log('bla bla bla bla');
-        callbackHablar();
-    }, 1000);
+function hablar(nombre){
+    return new Promise(( resolve, reject) => { //usamos la sintaxis ES6
+
+        setTimeout(function(){
+            console.log('bla bla bla bla');
+            resolve(nombre);
+        }, 1000);
+    });
+   
 }
 
-function adios(nombre, otroCallback) {
-    setTimeout(function(){
-        console.log('Adios' + nombre);
-        otroCallback();
-    }, 1500)
+function adios(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function(){
+            console.log('Adios' + nombre);
+            resolve();
+        }, 1000);
+    });
 }
+
+//Llamamos a la función
+console.log('Iniciando el proceso...');
+Hola('Ariel')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminando el proceso');
+    });
