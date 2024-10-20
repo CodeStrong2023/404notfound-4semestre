65a2c951e6a6f8592165692898c9fe6e4bbf0f1b
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-10-20 19:50:38 -0300

    Termino el boton modificar

--- Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Semana 6/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -106,7 +106,17 @@ public class LibroFrom extends JFrame {
                 libroTexto.requestFocusInWindow();
                 return;
             }
-
+            // Llenamos el objeto libro a actualizar.
+            int idLibro = Integer.parseInt(idTexto.getText());
+            var nombreLibro = libroTexto.getText();
+            var autor = autorTexto.getText();
+            var precio = Double.parseDouble(precioTexto.getText());
+            var existencias = Integer.parseInt(existenciasTexto.getText());
+            var libro = new Libro(idLibro, nombreLibro, autor, precio, existencias);
+            libroServicio.guardarLibro(libro);
+            mostrarMensaje("Se modifico el libro...");
+            limpiarFormulario();
+            listarLibros();
         }
     }
 
