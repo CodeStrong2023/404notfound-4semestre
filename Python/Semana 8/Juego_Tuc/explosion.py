e7f776a4572bd69e40fec3f002df1f39c45122fb
ignacio jose rocha <ignac1997nacho@gmail.com>
2024-10-20 03:28:04 -0300

    agrego video

--- Python/Semana 8/Juego_Tuc/explosion.py
+++ Python/Semana 8/Juego_Tuc/explosion.py
@@ -5,19 +5,21 @@ from constantes import ASSETS_PATH
 class Explosion:
 
     def __init__(self, x, y):
-        self.images = [pygame.image.load(os.path.join(ASSETS_PATH, "images/explosion0.png")) for in range (9)]
+        self.images = [pygame.image.load(f"{ASSETS_PATH}/images/regularExplosion0{i:02d}.png") for in range (9)]
         self.index = 0
         self.image = self.image.get_rect(center=(x, y))
         self.frame_rate=0
-        self.max_frames = 9
+        self.max_frames = 0
+
+        #actualizar la pantalla
 
     def actualizar(self):
-        self.frame_rate
-        self.frame_rate>=self.max_frames:
-        self.frame_rate=0
+        self.frame_rate+=1
+        if self.frame_rate>=self.max_frames:
+           self.frame_rate=0
         self.index+=1
         if self.index>=len(self.images):
             return False
         return True
     def dibujar(self, screen):
-        screen.blit(self.image, self.image.get_rect())  
+        screen.blit(self.image, self.rect.toplet)  
--- Python/Semana 8/Juego_Tuc/main.py
+++ Python/Semana 8/Juego_Tuc/main.py
@@ -1,12 +1,19 @@
 import sys
 import pygame
+import personaje
+from explosion import Explosion
+import random
+import constantes
+import os
 
+from personaje import *
+from personaje import Enemigo
 from constantes import SCREEN_WIDTH, SCREEN_HEIGTH ,ASSETS_PATH, IMPERIAL_MARCH_PATH,START_IMAGE_PATH, ESTRELLA_PATH, FONDO1_PATH
 
 def mostrar_pantalla_inicio(screen):
     # Cargar y mostrar la imágen de inicio
     imagen_inicio = pygame.image.load(START_IMAGE_PATH)
-    imagen_inicio = pygame.transform.scale(imagen_inicio, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    imagen_inicio = pygame.transform.scale(imagen_inicio, (SCREEN_WIDTH, SCREEN_HEIGTH))
     screen.blit(imagen_inicio, (0, 0))
     pygame.display.flip()
 
@@ -26,25 +33,25 @@ def mostrar_pantalla_inicio(screen):
 
     def main():
         pygame.init()
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
         pygame.display.set_caption('Amenaza Fantasma')
 
         # Cargar los recursos del juego
         icon=pygame.image.load(f'{ASSETS_PATH}/images/fondo001.jfif')
         pygame.display.set_icon(icon)
 
-        fondo = pygame.image.load(f'{ASSES_PATH}/images/fond3.jpg')
-        fondo = pygame.transform.scale(fondo, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        fondo = pygame.image.load(f'{ASSETS_PATH}/images/fond3.jpg')
+        fondo = pygame.transform.scale(fondo, (SCREEN_WIDTH, SCREEN_HEIGTH))
 
         estrella = pygame.image.load(ESTRELLA_PATH)
-        estrella = pygame.transform.scale(fondo1, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        estrella = pygame.transform.scale(fondo1, (SCREEN_WIDTH, SCREEN_HEIGTH))
 
         fondo1 = pygame.image.load(FONDO1_PATH)
-        fondo1 = pygame.transform.scale(fondo1( SCREEN_WIDTH, SCREEN_HEIGHT))
+        fondo1 = pygame.transform.scale(fondo1( SCREEN_WIDTH, SCREEN_HEIGTH))
 
         sonido_laser = pygame.mixer.Sound(f' {ASSETS_PATH}/sound/explocion.mp3')
 
-        personaje = Personaje (SCREEN_WIDTH //2, SCREEN_HEIGTH // 2)
+        personaje = personaje.Personaje (SCREEN_WIDTH //2, SCREEN_HEIGTH // 2)
         Enemigos = []
         explociones = []
         puntos = 0
@@ -84,7 +91,7 @@ def mostrar_pantalla_inicio(screen):
 
 for enemigo in enemigos:
     enemigo.mover()
-    if enemigo.rect.top > SCREEN_HEIGHT:
+    if enemigo.rect.top > SCREEN_HEIGTH:
         enemigo.remove(enemigo)
     for laser in personaje.lasers:
         if enemigo.rect.colliderect(laser.rect):
@@ -98,5 +105,44 @@ for enemigo in enemigos:
             if not personaje.recibir_daño():
                 running = False
 
-    
+        if random.random()<0.02:
+            x=random.randint(0,SCREEN_WIDTH-50)
+            enemigos=Enemigo(x,0)
+            enemigos.append(enemigo)
 
+        explosiones=[explosion for explosiones if explosionactualizar()]
+
+        if puntos>=250:
+            if fondo_actual==fondo:
+                fondo_actual=estrella
+            else:
+                fondo_actual=fondo1
+                puntos=0
+                nivel+=1 # Incrementar el nivel
+
+
+                screen.blit(fondo_actual,(0,0))
+                personaje.dibujar(screen)
+                for enemigo in enemigos:
+                    enemigo.dibujar(screen)
+                for explosion in explosiones:
+                    explosion.dibujar(screen)
+        #mostrar el marcador y el nivel
+        font=pygame.font.Fonto(None,36)
+        texto_puntos=font.render(f"Puntos: {puntos}",True,(255,255,255))
+        texto_nivel=font.render(f"Nivel: {nivel}",True,(255,255,255))
+        screen.blit(texto_puntos,(10,50))
+        screen.blit(texto_nivel,(10,90))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+        #mostrar mensaje de GAME OVER
+        screen.fill((0,0,0))
+        texto_game_over=font.render("GAME OVER",True,(255,0,0))
+        texto_mensaje_final=font.render("Presiona R para reiniciar",True,(255,255,255))
+
+        #mostrar freeze
+        screen.blit(texto_game_over,(SCREEN_WIDTH//2-texto_game_over.get_width()//2,SCREEN_HEIGTH//2-texto_game_over.get_height()//2))
+        pygame.display.flip()
+        pygame.time.wait(2000)#muestra la frase de game over por dos segundos
