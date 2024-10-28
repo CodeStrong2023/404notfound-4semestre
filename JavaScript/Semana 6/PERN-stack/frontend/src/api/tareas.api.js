d6f049e01ea4c75d93385f5fb57e5d24f52cb974
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-10-28 20:10:43 -0300

    Agregamos parte del video 10 de la clase 10

--- JavaScript/Semana 6/PERN-stack/frontend/src/api/tareas.api.js
+++ JavaScript/Semana 6/PERN-stack/frontend/src/api/tareas.api.js
@@ -1,3 +1,5 @@
 import axios from "./axios"
 
 export const crearTareaRequest = async (Tarea) => {await axios.post("/tareas", tarea);}
+
+export const obtenerTareasResquest = async () => {axios.get}
