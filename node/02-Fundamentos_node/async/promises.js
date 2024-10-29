102f8351bd0614bf583663f6580a83616f3d52fa
Sebastian Betancurth <33584221+SebastianBetancurth@users.noreply.github.com>
2024-10-28 21:15:27 -0300

    Agregando async hablar, hola , adios - llamada a la funcion

--- node/02-Fundamentos_node/async/promises.js
+++ node/02-Fundamentos_node/async/promises.js
@@ -1,13 +1,14 @@
-function Hola(nombre) {
+async function Hola(nombre) {
     return new Promise((resolve, reject) => {
         setTimeout(function() {
             console.log('Hola ' + nombre);
             resolve(nombre); // Resolvemos con el nombre para pasarlo a las siguientes promesas
         }, 1000);
     });
+
 }
 
-function hablar(nombre) {
+async function hablar(nombre) {
     return new Promise((resolve, reject) => {
         setTimeout(function() {
             console.log('bla bla bla bla');
@@ -16,11 +17,14 @@ function hablar(nombre) {
     });
 }
 
-function adios(nombre) {
+
+
+async function adios(nombre) {
     return new Promise((resolve, reject) => {
         setTimeout(function() {
             console.log('Adios ' + nombre);
-            resolve(); // No es necesario pasar el nombre en la última función
+            //resolve(); // No es necesario pasar el nombre en la última función
+            reject('Hay un error')
         }, 1000);
     });
 }
@@ -32,9 +36,23 @@ Hola('Ariel')
     .then(hablar)
     .then(hablar)
     .then(adios)
-    .then(() => {
+    .then((nombre) => {
         console.log('Terminando el proceso');
     })
     .catch((error) => {
-        console.error('Error:', error);
+        console.log('Ha habido un error');
+        console.log(error);
     });
+
+
+async function main() {
+    let nombre = await hola('Sebas');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    
+    
+} 
+
+main ();
