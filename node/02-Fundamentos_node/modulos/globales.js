3ed2cfc031a43873702566c3c669b3ec69363cf3
Matias Landi <landi.matias7@gmail.com>
2024-10-30 20:50:08 -0300

    Agrego video 1 de la clase 12 de Node

---
+++ node/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,16 @@
+// this === global = true
+
+// Mostrar algo en consola
+console.log();
+
+// Mostrar un mensaje en forma de error
+console.error();
+
+// Ejecutar un código después de un intervalo de tiempo
+setTimeout(() => {});
+
+// Ejecutar un código en intervalo de tiempo
+setInterval(() => {});
+
+// Da prioridad de ejecución a una función asincrónica
+setImmediate(() => {});
