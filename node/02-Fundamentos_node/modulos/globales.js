2f55a3abfd38dd575cb326477190a82c01b85a5a
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-10-31 16:28:56 -0300

    Modulos global setInterval

--- node/02-Fundamentos_node/modulos/globales.js
+++ node/02-Fundamentos_node/modulos/globales.js
@@ -1,16 +1,18 @@
 // this === global = true
 
 // Mostrar algo en consola
-console.log();
+//console.log();
 
 // Mostrar un mensaje en forma de error
-console.error();
+//console.error();
 
 // Ejecutar un código después de un intervalo de tiempo
-setTimeout(() => {});
+//setTimeout(() => {});
 
 // Ejecutar un código en intervalo de tiempo
-setInterval(() => {});
+//setInterval(() => {});
 
 // Da prioridad de ejecución a una función asincrónica
-setImmediate(() => {});
+//setImmediate(() => {});
+
+console.log(setInterval);
