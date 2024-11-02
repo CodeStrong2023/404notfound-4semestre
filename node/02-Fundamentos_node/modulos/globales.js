2368ecbb9673c982ffcdcd72619e41d3125839bf
Sebas <sebasbeta-11@hotmail.com>
2024-11-02 10:36:09 -0300

    Agrego mi variable global,en globales.js

---
+++ node/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,38 @@
+// this === global = true
+
+// Mostrar algo en consola
+//console.log();
+
+// Mostrar un mensaje en forma de error
+//console.error();
+
+// Ejecutar un código después de un intervalo de tiempo
+//setTimeout(() => {});
+
+// Ejecutar un código en intervalo de tiempo
+//setInterval(() => {});
+
+// Da prioridad de ejecución a una función asincrónica
+//setImmediate(() => {});
+
+//console.log(setInterval);
+
+let i = 0;
+let intervalo = setInterval(() => {
+    console.lohg('Hola');
+    if (i === 3) {
+        clearInterval(intervalo); // detenemos la funcion
+    }
+    i++;
+}, 1000);
+
+setImmediate(() => {
+    console.log('Saludo inmediato');
+});
+
+//require();
+
+console.log(__filename);
+
+global.miVariable = 'mi variable global';
+console.log(miVariable);
