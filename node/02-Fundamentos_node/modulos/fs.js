bd6e2167342497ca7c2b6b98bfddc1f6f44db455
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-11-08 18:09:27 -0300

    Modulos file system video 3

--- node/02-Fundamentos_node/modulos/fs.js
+++ node/02-Fundamentos_node/modulos/fs.js
@@ -7,5 +7,17 @@ function leer(ruta, cb){
     })
 }
 
-leer('${__dirname}/archivo.txt', console.log);// Sintaxis ES6
 
+// Segundo escribimos el archivo1.txt crandolo.
+function escribir(ruta, contenido, cb) {
+    fs.writeFile(ruta, contenido, function(err){
+        if (err) {
+            console.log("No se ha podido escribir", err);
+        } else {
+            console.log("Se ha escrito correctamente");
+        }
+    })
+}
+
+//escribir(`${__dirname}/archivo1.txt`, "Reescribimos el archivo", console.log);
+leer('${__dirname}/archivo.txt', console.log);// Sintaxis ES6
